14b5c5498be4c8740874a64b7f6033c17bc8c541
JuliBorn <juliusbornmuc@gmail.com>
2023-02-13 14:49:06 +0700

    stuff

--- components/Article.js
+++ components/Article.js
@@ -1,38 +1,37 @@
-import React from 'react';
+import React from 'react'
 
-import Link from 'next/link';
+import Link from 'next/link'
 
-import Image from 'next/image';
-import { Fade } from 'react-reveal';
-
+import Image from 'next/image'
+import { Fade } from 'react-reveal'
 
 const Article = (props) => {
-    return (
-        <>
-            <Fade>
-                <Link href={props.link} passHref>
+  //Comment
+  return (
+    <>
+      <Fade>
+        <Link href={props.link} passHref>
+          <div className="card_content">
+            <img
+              src={`/static/images/${props.pic_url}`}
+              // width='600px'
+              // height='450px'
+              layout="fill"
+              objectFit="cover"
+              z-index={101}
+              className="content_image"
+              opacity={0.5}
+              alt=""
+            ></img>
+            <div className="mid noHover">
+              <h3>{props.name}</h3>
+              <h4>{props.subheading1}</h4>
+            </div>
+          </div>
+        </Link>
+      </Fade>
+    </>
+  )
+}
 
-                    <div className='card_content'>
-                        <img
-                            src={`/static/images/${props.pic_url}`}
-                            // width='600px'
-                            // height='450px'
-                            layout="fill"
-                            objectFit="cover"
-                            z-index={101}
-                            className='content_image'
-                            opacity={0.5}
-                            alt=''
-                        ></img>
-                        <div className='mid noHover'>
-                            <h3>{props.name}</h3>
-                            <h4>{props.subheading1}</h4>
-                        </div>
-                    </div>
-                </Link>
-            </Fade>
-        </>
-    );
-};
-
-export default Article;
+export default Article
