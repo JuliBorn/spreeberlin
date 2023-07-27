bcaa3e7a72c36d735027dfe8bc937afef5a66cc2
JuliBorn <juliusbornmuc@gmail.com>
2023-07-27 15:15:58 +0200

    fixed some string whitespace issue

--- infoText.js
+++ infoText.js
@@ -1,38 +1,7 @@
 const infoText = {
     'info': {
-        'de': `Im Kontext der Klimakrise und des damit einhergehenden
-        gesellschaftlichen Wandels untersucht das Projekt die
-        Beziehung zwischen Berlin und der Spree, dem städtischen Raum,
-        den Bürgern und dem Gewässer.  <br\> Wie kann dem Fluss eine eigene
-        "politische" Stimme gegeben werden und was kann dazu
-        beitragen, mehr Bewusstsein für die Spree und den Einfluss des
-        Menschen zu schaffen? Mit Hilfe der Bojen macht das Gewässer
-        auf die Verschmutzung aufmerksam. Digitale Werkzeuge und
-        Medien, wie Messgeräte und App, erfassen den aktuellen Zustand
-        des Flusses und vermitteln ihn den Bürgern auf zugängliche
-        Weise. Spannende historische und aktuelle Artikel rund um das
-        Thema werden ebenfalls kommuniziert. Eine Zukunftsebene auf
-        der Website zeigt die ungenutzten Potenziale auf und
-        veranschaulicht, wie sich die Stadt mit Hilfe des Flusses zum
-        Besseren verändern könnte, und regt Bürger und Politiker zum
-        Handeln an. In den heißen Sommermonaten pumpt die Boje
-        Sauerstoff in den Fluss und versorgt die Spree und ihre
-        Lebewesen damit. Der planetenzentrierte Ansatz bietet eine
-        ganzheitliche und "grüne" Lösung für die verschiedenen
-        Probleme und sorgt für ein gesünderes Gleichgewicht zwischen
-        den beteiligten Akteuren.`,
-        'en': `In the context of the climate crisis and the associated societal change, 
-        the project explores the relationship between Berlin and the River Spree, the urban space,
-        the citizens, and the water. How can the river be given its own "political" voice, and what
-        can contribute to raising awareness of the Spree and the impact of humans? Through the use
-        of buoys, the water draws attention to pollution. Digital tools and media, such as measuring
-        devices and an app, capture the current state of the river and communicate it to the citizens in
-        an accessible way. Exciting historical and current articles on the topic are also shared. A future
-        dimension on the website highlights untapped potentials and illustrates how the city could change
-        for the better with the help of the river, encouraging citizens and politicians to take action.
-        During the hot summer months, the buoy pumps oxygen into the river, providing the Spree and its organisms with oxygen.
-        The planet-centered approach offers a holistic and "green" solution
-        to various problems and ensures a healthier balance among the involved actors.`,
+        de: `Im Kontext der Klimakrise und des damit einhergehenden gesellschaftlichen Wandels untersucht das Projekt die Beziehung zwischen Berlin und der Spree, dem städtischen Raum, den Bürgern und dem Gewässer. Wie kann dem Fluss eine eigene "politische" Stimme gegeben werden und was kann dazu beitragen, mehr Bewusstsein für die Spree und den Einfluss des Menschen zu schaffen? Mit Hilfe der Bojen macht das Gewässer auf die Verschmutzung aufmerksam. Digitale Werkzeuge und Medien, wie Messgeräte und App, erfassen den aktuellen Zustand des Flusses und vermitteln ihn den Bürgern auf zugängliche Weise. Spannende historische und aktuelle Artikel rund um das Thema werden ebenfalls kommuniziert. Eine Zukunftsebene auf der Website zeigt die ungenutzten Potenziale auf und veranschaulicht, wie sich die Stadt mit Hilfe des Flusses zum Besseren verändern könnte, und regt Bürger und Politiker zum Handeln an. In den heißen Sommermonaten pumpt die Boje Sauerstoff in den Fluss und versorgt die Spree und ihre Lebewesen damit. Der planetenzentrierte Ansatz bietet eine ganzheitliche und "grüne" Lösung für die verschiedenen Probleme und sorgt für ein gesünderes Gleichgewicht zwischen den beteiligten Akteuren.`,
+        en: `In the context of the climate crisis and the associated societal change, the project explores the relationship between Berlin and the River Spree, the urban space, the citizens, and the water. How can the river be given its own "political" voice, and what can contribute to raising awareness of the Spree and the impact of humans? Through the use of buoys, the water draws attention to pollution. Digital tools and media, such as measuring devices and an app, capture the current state of the river and communicate it to the citizens in an accessible way. Exciting historical and current articles on the topic are also shared. A future dimension on the website highlights untapped potentials and illustrates how the city could change for the better with the help of the river, encouraging citizens and politicians to take action. During the hot summer months, the buoy pumps oxygen into the river, providing the Spree and its organisms with oxygen. The planet-centered approach offers a holistic and "green" solution to various problems and ensures a healthier balance among the involved actors.`,
     },
     'relevanz': {
         'de': `Während der umfangreichen Recherchen
@@ -92,7 +61,6 @@ const infoText = {
             das Ökostromnetz der Stadt, zum anderen durch menschliche Kraft (Sportgeräte o.ä.) erfolgen.`,
         'en': `The buoy floats in the Spree Canal and provides oxygen to the water. Above the water surface, the water quality is reflected. The energy supply can be sourced from the city's renewable energy grid and, alternatively, from human power (such as exercise equipment or similar).`,
     },
-    
 };
 
 export default infoText;
